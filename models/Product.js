0c5b20474d12716c8ce2e93abde54c03abd2f6b7
Phan Quang Minh Long <60604069+minhlong26052001@users.noreply.github.com>
2022-05-19 10:17:58 +0700

    first commit

---
+++ models/Product.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    
+    category: {
+        type: String,
+        // required: true
+    },
+    label: {
+        type: String,
+        // required: true
+    },
+    name: {
+        type: String,
+        // required: true
+    },
+    image: {
+        type: String,
+        // required: true
+    },
+    weight: {
+        type: String,
+        // required: true
+    },
+    color: {
+        type: String,
+        // required: true
+    },
+    price: {
+        type: Number,
+        // required: true
+    },
+    description: {
+        type: String,
+        // required: true
+    },
+    benefit: {
+        type: String,
+        // required: true
+    },
+    instruction: {
+        type: String,
+        // required: true
+    },
+    createdAt: {type:Date, default:Date.now},
+    updatedAt: {type:Date, default:Date.now}
+})
+
+module.exports = mongoose.model("Product", ProductSchema);
